62a3790396829d075c35b197955b3ef814150003
andylin2004 <alin21@stuy.edu>
2021-01-05 17:06:19 -0500

    added add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,16 +2,34 @@ class MyLinkedList{
  private int size;
  private Node start,end;
 
- public int size(){
-   return this.size;
- }
- public boolean add(String value){
-   return;
- }
- public boolean add(int index, String value){
-   return;
- }
- public String toString(){
-   return;
- }
+  public MyLinkedList(){
+    size = 0;
+  }
+  public int size(){
+    return this.size;
+  }
+  public boolean add(String value){
+    if (this.size == 0){
+      this.start = new Node(value);
+    }else{
+      Node temp = this.end;
+      this.end = new Node(value);
+      temp.setNext(this.end);
+      this.end.setPrev(temp);
+    }
+    this.size++;
+    return true;
+  }
+  public void add(int index, String value){
+    
+  }
+  public String get(int index){
+    return
+  }
+  public String set(int index, String value){
+    return
+  }
+  public String toString(){
+    return;
+  }
 }
