8eedcb4ff53c970b359a633c30189dbf8d8eb2a3
Andy Lin <alin21@stuy.edu>
2021-01-05 13:23:02 -0500

    added reading funcs and setnext

--- Node.java
+++ Node.java
@@ -8,4 +8,16 @@ class Node{
  public void setPrev(Node in){
    this.prev = in;
  }
+ public void setNext(Node in){
+   this.next = in;
+ }
+ public String getData(){
+   return this.data;
+ }
+ public String getPrev(){
+   return prev.getData()
+ }
+ public String getNext(){
+   return next.getData();
+ }
 }
