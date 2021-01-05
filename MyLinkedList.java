0aff1bd91aad5608f961a5753e95239f0066efbc
Andy Lin <alin21@stuy.edu>
2021-01-05 13:14:55 -0500

    setup p2

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,8 +2,16 @@ class MyLinkedList{
  private int size;
  private Node start,end;
 
- public int size();
- public boolean add(String value);
- public boolean add(int index, String value);
- public String toString();
+ public int size(){
+   return this.size;
+ }
+ public boolean add(String value){
+   return;
+ }
+ public boolean add(int index, String value){
+   return;
+ }
+ public String toString(){
+   return;
+ }
 }
